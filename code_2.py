809a9b3abd7930f346d378b95c646ca7cd7ae072
Judy <143587407+Judy770@users.noreply.github.com>
2024-01-06 11:08:58 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+        console.log("Random Number: " + item);
+    });
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+
+
+    const data = generateRandomData();
+    const data = [];
+    return data;
+function generateRandomData() {
+main();
+    }
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+}
